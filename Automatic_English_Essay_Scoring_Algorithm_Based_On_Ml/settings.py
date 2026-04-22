6ed36fd67cb16ff2cb060c5729004f77b3b18205
Tejasri <tejasrichegondi25@users.noreply.github.com>
2026-04-22 10:30:38 +0530

    Enabled WhiteNoise DEV static finders to fix Render missing assets

--- Automatic_English_Essay_Scoring_Algorithm_Based_On_Ml/settings.py
+++ Automatic_English_Essay_Scoring_Algorithm_Based_On_Ml/settings.py
@@ -140,6 +140,7 @@ STATIC_URL = 'static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = [os.path.join(BASE_DIR,'static')]
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
+WHITENOISE_USE_FINDERS = True
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
